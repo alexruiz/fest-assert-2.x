e9a109e091aaff6bc88f515e83326ab307949a3b
Yvonne Wang <wanghy1101@gmail.com>
2013-06-03 12:52:11 -0700

    Fixing tests.

--- src/test/java/org/fest/assertions/api/ByteAssert_isGreaterThan_Byte_Test.java
+++ src/test/java/org/fest/assertions/api/ByteAssert_isGreaterThan_Byte_Test.java
@@ -1,19 +1,18 @@
 /*
+ * Created on Mar 29, 2009
  *
- *  * Created on Dec 16, 2010
- *  *
- *  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
- *  * License. You may obtain a copy of the License at
- *  *
- *  * http://www.apache.org/licenses/LICENSE-2.0
- *  *
- *  * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
- *  * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
- *  * governing permissions and limitations under the License.
- *  *
- *  * Copyright @2013 the original author or authors.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Copyright @2013 the original author or authors.
  */
+
 package org.fest.assertions.api;
 
 import org.fest.test.ExpectedException;
@@ -25,16 +24,16 @@ import static org.fest.test.ExpectedException.none;
 import static org.junit.Assert.assertSame;
 
 /**
- * Tests for {@link ByteAssert#isGreaterThan(Comparable)}.
+ * Tests for {@link ByteAssert#isGreaterThan(byte)}.
  *
  * @author Yvonne Wang
  */
-public class ByteAssert_isGreaterThan_Byte_Test {
+public class ByteAssert_isGreaterThan_byte_Test {
   @Rule
   public ExpectedException thrown = none();
   private ByteAssert assertions;
   private Byte actual = 8;
-  private Byte expected = (byte) 6;
+  private byte expected = (byte) 6;
 
   @Before
   public void setUp() {
@@ -56,13 +55,6 @@ public class ByteAssert_isGreaterThan_Byte_Test {
   public void should_throw_error_if_actual_is_null() {
     thrown.expect(AssertionError.class);
     actual = null;
-    assertions.isGreaterThan(expected);
-  }
-
-  @Test
-  public void should_throw_error_if_expected_is_null() {
-    thrown.expect(AssertionError.class);
-    expected = null;
     assertions.isGreaterThan(expected);
   }
 
